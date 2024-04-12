72b4757c8cc3ad08c2da282386d4cbd439860a40
Yassine Moussaif <yassine.moussaif1234@gmail.com>
2024-04-12 19:39:11 +0200

    "labday5"

--- tests/add.spec.js
+++ tests/add.spec.js
@@ -19,6 +19,13 @@ describe("Iteration 1 | Getting Started", () => {
         expect(add()).toEqual(undefined);
         expect(add(undefined, 1)).toEqual(undefined);
       });
+      it("should return undefined if any of the two arguments is not a number",() =>{
+        expect(add("hello","1")).toEqual(undefined);
+        expect(add("1",1)).toEqual(undefined);
+        expect(add(1,"1")).toEqual(undefined);
+        expect(add([2,3],"1")).toEqual(undefined);
+      })
     });
   });
+
   
